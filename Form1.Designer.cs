d80f2869605c52ed9914cb4c5c661cb6b1c92c40
Vegh Adam <veghadam1991@gmail.com>
2016-04-20 08:23:25 +0200

    added driver browser

---
+++ Form1.Designer.cs
@@ -0,0 +1,152 @@
+﻿namespace Week14B_Monday_Driver
+{
+	partial class Form1
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.listBoxFiles = new System.Windows.Forms.ListBox();
+			this.comboBoxDrive = new System.Windows.Forms.ComboBox();
+			this.label1 = new System.Windows.Forms.Label();
+			this.label2 = new System.Windows.Forms.Label();
+			this.textBoxPath = new System.Windows.Forms.TextBox();
+			this.buttonOpen = new System.Windows.Forms.Button();
+			this.buttonDelete = new System.Windows.Forms.Button();
+			this.buttonBack = new System.Windows.Forms.Button();
+			this.SuspendLayout();
+			// 
+			// listBoxFiles
+			// 
+			this.listBoxFiles.FormattingEnabled = true;
+			this.listBoxFiles.Location = new System.Drawing.Point(28, 110);
+			this.listBoxFiles.Name = "listBoxFiles";
+			this.listBoxFiles.Size = new System.Drawing.Size(426, 173);
+			this.listBoxFiles.TabIndex = 0;
+			this.listBoxFiles.SelectedIndexChanged += new System.EventHandler(this.listBoxFiles_SelectedIndexChanged);
+			// 
+			// comboBoxDrive
+			// 
+			this.comboBoxDrive.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+			this.comboBoxDrive.FormattingEnabled = true;
+			this.comboBoxDrive.Location = new System.Drawing.Point(121, 22);
+			this.comboBoxDrive.Name = "comboBoxDrive";
+			this.comboBoxDrive.Size = new System.Drawing.Size(333, 21);
+			this.comboBoxDrive.Sorted = true;
+			this.comboBoxDrive.TabIndex = 1;
+			this.comboBoxDrive.SelectedIndexChanged += new System.EventHandler(this.comboBoxDrive_SelectedIndexChanged);
+			// 
+			// label1
+			// 
+			this.label1.AutoSize = true;
+			this.label1.Location = new System.Drawing.Point(64, 25);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(32, 13);
+			this.label1.TabIndex = 2;
+			this.label1.Text = "Drive";
+			// 
+			// label2
+			// 
+			this.label2.AutoSize = true;
+			this.label2.Location = new System.Drawing.Point(64, 69);
+			this.label2.Name = "label2";
+			this.label2.Size = new System.Drawing.Size(29, 13);
+			this.label2.TabIndex = 3;
+			this.label2.Text = "Path";
+			// 
+			// textBoxPath
+			// 
+			this.textBoxPath.Enabled = false;
+			this.textBoxPath.Location = new System.Drawing.Point(121, 66);
+			this.textBoxPath.Name = "textBoxPath";
+			this.textBoxPath.Size = new System.Drawing.Size(333, 20);
+			this.textBoxPath.TabIndex = 4;
+			// 
+			// buttonOpen
+			// 
+			this.buttonOpen.Location = new System.Drawing.Point(28, 308);
+			this.buttonOpen.Name = "buttonOpen";
+			this.buttonOpen.Size = new System.Drawing.Size(75, 23);
+			this.buttonOpen.TabIndex = 5;
+			this.buttonOpen.Text = "Open";
+			this.buttonOpen.UseVisualStyleBackColor = true;
+			this.buttonOpen.Click += new System.EventHandler(this.buttonOpen_Click);
+			// 
+			// buttonDelete
+			// 
+			this.buttonDelete.Location = new System.Drawing.Point(109, 308);
+			this.buttonDelete.Name = "buttonDelete";
+			this.buttonDelete.Size = new System.Drawing.Size(75, 23);
+			this.buttonDelete.TabIndex = 6;
+			this.buttonDelete.Text = "Delete";
+			this.buttonDelete.UseVisualStyleBackColor = true;
+			this.buttonDelete.Click += new System.EventHandler(this.buttonDelete_Click);
+			// 
+			// buttonBack
+			// 
+			this.buttonBack.Location = new System.Drawing.Point(190, 308);
+			this.buttonBack.Name = "buttonBack";
+			this.buttonBack.Size = new System.Drawing.Size(75, 23);
+			this.buttonBack.TabIndex = 7;
+			this.buttonBack.Text = "Back";
+			this.buttonBack.UseVisualStyleBackColor = true;
+			this.buttonBack.Click += new System.EventHandler(this.buttonBack_Click);
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(484, 361);
+			this.Controls.Add(this.buttonBack);
+			this.Controls.Add(this.buttonDelete);
+			this.Controls.Add(this.buttonOpen);
+			this.Controls.Add(this.textBoxPath);
+			this.Controls.Add(this.label2);
+			this.Controls.Add(this.label1);
+			this.Controls.Add(this.comboBoxDrive);
+			this.Controls.Add(this.listBoxFiles);
+			this.MaximumSize = new System.Drawing.Size(500, 400);
+			this.MinimumSize = new System.Drawing.Size(500, 400);
+			this.Name = "Form1";
+			this.Text = "Editing";
+			this.Load += new System.EventHandler(this.Form1_Load);
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.ListBox listBoxFiles;
+		private System.Windows.Forms.ComboBox comboBoxDrive;
+		private System.Windows.Forms.Label label1;
+		private System.Windows.Forms.Label label2;
+		private System.Windows.Forms.TextBox textBoxPath;
+		private System.Windows.Forms.Button buttonOpen;
+		private System.Windows.Forms.Button buttonDelete;
+		private System.Windows.Forms.Button buttonBack;
+	}
+}
+
---
+++ Form1.cs
@@ -0,0 +1,142 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Collections;
+
+namespace Week14B_Monday_Driver
+{
+	public partial class Form1 : Form
+	{
+		public DriveInfo[] Drives { get; private set; }
+		public string currentDrive;
+		public DirectoryInfo currentDir;
+		public FileInfo[] currentFiles;
+		public DirectoryInfo[] currentDirs;
+		public FileSystemInfo selectedFile;
+		public bool isDirectory;
+
+		public Form1()
+		{
+			InitializeComponent();
+		}
+
+		private void Form1_Load(object sender, EventArgs e)
+		{
+			Drives = DriveInfo.GetDrives();
+			comboBoxDrive.Items.AddRange(Drives);
+		}
+
+		private void setupItems(DirectoryInfo currentDir)
+		{
+			try
+			{
+				listBoxFiles.Items.Clear();
+				currentFiles = currentDir.GetFiles();
+				currentDirs = currentDir.GetDirectories();
+				currentDir = currentDir.Parent;
+				listBoxFiles.Items.AddRange(currentFiles);
+				listBoxFiles.Items.AddRange(currentDirs);
+			}
+			catch (Exception)
+			{
+				return;
+			}
+		}
+
+		private void comboBoxDrive_SelectedIndexChanged(object sender, EventArgs e)
+		{
+			try
+			{
+				textBoxPath.Text = comboBoxDrive.SelectedItem.ToString();
+				currentDrive = textBoxPath.Text;
+				listBoxFiles.Items.Clear();
+				selectedFile = null;
+				currentDir = Directory.CreateDirectory(currentDrive);
+				setupItems(currentDir);
+			}
+			catch (DirectoryNotFoundException)
+			{
+				MessageBox.Show("This drive has no directories!", "Warning!");
+			}
+		}
+
+		private void listBoxFiles_SelectedIndexChanged(object sender, EventArgs e)
+		{
+			try
+			{
+				selectedFile = (FileSystemInfo)listBoxFiles.SelectedItem;
+				textBoxPath.Text = selectedFile.FullName;
+
+				if ((selectedFile.Attributes & FileAttributes.Directory) == FileAttributes.Directory)
+					isDirectory = true;
+				else
+					isDirectory = false;
+			}
+			catch (Exception)
+			{
+				return;
+			}
+		}
+
+		private void buttonOpen_Click(object sender, EventArgs e)
+		{
+			if (isDirectory && selectedFile != null)
+			{
+				currentDir = (DirectoryInfo)selectedFile;
+				setupItems(currentDir);
+			}
+
+			else if (!isDirectory && selectedFile != null)
+			{
+				try
+				{
+					MessageBox.Show(File.ReadAllText(selectedFile.FullName), "Hello");
+				}
+				catch (Exception ex)
+				{
+					MessageBox.Show(ex.StackTrace, "Hello");
+				}
+			}
+			else
+				return;
+		}
+
+		private void buttonBack_Click(object sender, EventArgs e)
+		{
+			try
+			{
+				currentDir = currentDir.Parent;
+				MessageBox.Show(currentDir.FullName);
+				textBoxPath.Text = currentDir.FullName;
+				setupItems(currentDir);
+			}
+			catch (Exception)
+			{
+				selectedFile = null;
+				listBoxFiles.Items.Clear();
+				return;
+			}
+		}
+
+		private void buttonDelete_Click(object sender, EventArgs e)
+		{
+			string deleteFile = selectedFile == null ? null : "Delete file: " + selectedFile.Name;
+			if (deleteFile != null && !isDirectory && (MessageBox.Show("Are sure in your selection?", deleteFile, MessageBoxButtons.YesNo, MessageBoxIcon.Warning) == DialogResult.Yes))
+			{
+				selectedFile.Delete();
+				setupItems(currentDir);
+				textBoxPath.Text = currentDir.FullName;
+				selectedFile = null;
+			}
+			else
+				return;
+		}
+	}
+}
